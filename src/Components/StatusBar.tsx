ee2439024daecffbda211b51002ad7ea25831183
cooljohnny3 <cooljohnny3@gmail.com>
2020-06-01 11:17:46 -0700

    Money turns red when negative

--- src/Components/StatusBar.tsx
+++ src/Components/StatusBar.tsx
@@ -1,9 +1,9 @@
-// Status to display name of play and current money count
+// Status to display name of player and current money count
 
 import * as React from 'react'
 
 export interface StatusProps { name: string, money: number };
 
 export function StatusBar(props: StatusProps) {
-    return <p className="statusbar">{props.name} : {props.money}</p>
+    return <p className="statusbar">{props.name} : <span style={{color: (props.money < 0) ?'red' : 'yellow'}}>{props.money}</span></p>
 }
